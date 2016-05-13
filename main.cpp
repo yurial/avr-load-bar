ab072cb8540952bddc8fd7e203d584e686c05624
Yuriy Dyachenko <pub.yurial@gmail.com>
2016-05-13 22:23:22 +0300

    use bar

--- main.cpp
+++ main.cpp
@@ -7,71 +7,10 @@
 #define PIN_CLK     2
 #define PIN_LOAD    3
 
-class max7219
+void bar(uint8_t v)
     {
-    public:
-    enum cmd
-        {
-        noop        = 0x00,
-        digit0      = 0x01,
-        digit1      = 0x02,
-        digit2      = 0x03,
-        digit3      = 0x04,
-        digit4      = 0x05,
-        digit5      = 0x06,
-        digit6      = 0x07,
-        digit7      = 0x08,
-        decodeMode  = 0x09,
-        intensity   = 0x0a,
-        scanLimit   = 0x0b,
-        shutdown    = 0x0c,
-        displayTest = 0x0f
-        };
-    };
-
-void shiftOut(uint8_t data)
-    {
-    for (uint8_t i = 0; i < 8; i++)
-        {
-        if ( data & 0x80 )
-            {
-            PORTB = _BV( PIN_DATA );
-            PORTB = _BV( PIN_DATA ) | _BV( PIN_CLK );
-            }
-        else
-            {
-            PORTB = 0; 
-            PORTB = _BV( PIN_CLK );
-            }
-        data <<= 1;
-        PORTB = 0; 
-        }
-    }
-
-void send_cmd(uint8_t cmd, uint8_t val)
-    {
-    PORTB = 0;
-    shiftOut( cmd );
-    shiftOut( val );
-    PORTB = _BV( PIN_LOAD );
-    }
-
-void print(uint8_t n3, uint8_t n2, uint8_t n1, uint8_t n0)
-    {
-    send_cmd( max7219::shutdown, 0x00 );
-    send_cmd( max7219::intensity, 0x0F );
-    send_cmd( max7219::decodeMode, 0xFF );
-    send_cmd( max7219::scanLimit, 0x04 );
-    send_cmd( max7219::digit0, n0 );
-    send_cmd( max7219::digit1, n1 );
-    send_cmd( max7219::digit2, n2 );
-    send_cmd( max7219::digit3, n3 );
-    send_cmd( max7219::shutdown, 0x01 );
-    }
-
-void print(uint16_t v)
-    {
-    print( v%10, v/10%10, v/100%10, v/1000%10 );
+    PORTC = (v>6)? 0xFF : (0xFF << (6 - v));
+    PORTB = 0xFF << (12 - v);
     }
 
 uint16_t next_pow2(uint16_t x)
@@ -88,18 +27,26 @@ uint16_t next_pow2(uint16_t x)
 ISR(TIMER0_OVF_vect,ISR_BLOCK)
     {
     static uint16_t clock_max = 1;
+    static float clock_10percent = 1.0;
     uint16_t clock_val = TCNT1>>3;
     TCNT1 = 0;
     if ( clock_val > clock_max )
-        clock_max = next_pow2( clock_val );
-    print( ceil(static_cast<float>(clock_max-clock_val)/clock_max*100.0) ); //16383
+        {
+        clock_max = next_pow2( clock_val )-1;
+        clock_10percent = static_cast<float>( clock_max )/10.0;
+        }
+    bar( round(static_cast<float>(clock_max-clock_val)/clock_10percent) );
     }
 
 int main()
     {
-    DDRB = _BV(PIN_IDLE) | _BV(PIN_DATA) | _BV(PIN_CLK) | _BV(PIN_LOAD);
-    PORTB = _BV(PIN_LOAD);
-    sei();
+    DDRB = 0xFF;
+    DDRC = 0xFF;
+    for (int i = 0; i < 11; ++i)
+        {
+        bar( i );
+        _delay_ms( 100 );
+        }
     //TIMER1
     TCCR1B = _BV(CS12) | _BV(CS11); //External clock source on T1 pin. Clock on falling edge
     //TIMER0
@@ -107,10 +54,9 @@ int main()
     TCNT0 = 0;
     TCNT1 = 0;
     TIMSK |= _BV(TOIE0); //overflow interrupt
+    sei();
     for (;;)
         {
-        PORTB = _BV(PIN_LOAD) | _BV(PIN_IDLE);
-        PORTB = _BV(PIN_LOAD);
         }
 
     return 0;
